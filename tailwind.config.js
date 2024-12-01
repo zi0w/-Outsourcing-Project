841dd15a9f9d2ea15fbdc1e687a72d5920ef7139
Noon_sae <minseock7308@gmail.com>
2024-12-02 07:33:27 +0900

    add: backgroundImage - gradient-45

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
-    extend: {}
+    extend: {
+      backgroundImage: {
+        'gradient-45':'linear-gradient(to right bottom, #0e0e0e, #333);'
+      }      
+    }
   },
   plugins: []
 };
