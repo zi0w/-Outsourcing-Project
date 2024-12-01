69d2ecfcba4b5dce457bb59485f573ae2c8fcbc8
Noon_sae <minseock7308@gmail.com>
2024-12-02 07:38:15 +0900

    add: header style

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -3,56 +3,53 @@ import { Link, useNavigate } from 'react-router-dom';
 const Header = () => {
   // navigate = useNavigate();
 
-  const h1 = styled.h1`
-    display: block;
-    width: 64px;
-    height: 64px;
-    background: url()
-
-    font-size: 34px;
-    line-height: 64px;
-
-  `
-
   return (
     <header>
-      <div className="w-full max-w-[1440px] h-[100px] mx-auto bg-black text-white flex flex-row justify-between items-center">
+      <div className="w-full max-w-[1440px] h-[100px] mx-auto bg-black text-white flex flex-row justify-between items-center pr-[15px]">
         {/* logo */}
-        <h1 className="w-[64px] h-[64px]">이븐한 맛집</h1>
+        <Link to="/" className="flex">
+          <h1 className="block w-16 h-16 bg-[url('/logo.png')] ml-[25px] bg-no-repeat bg-center bg-cover"></h1>
+          <span className="text-[34px] leading-[64px] ml-[12px] ">이븐한맛집</span>
+        </Link>
 
         {/* nav */}
-        <ul>
+        <ul className="flex text-[26px]">
           <li>
             {/* go to search page */}
-            <Link to="">전체보기</Link>
+            <Link to="/search" className="content-box p-[10px] hover:text-red-500">
+              전체보기
+            </Link>
           </li>
-          <li>
+          <li className="mx-[15px] my-0">
             {/* go to signIn page */}
-            <Link to="">로그인</Link>
+            <Link to="/signin" className="content-box p-[10px] hover:text-red-500">
+              로그인
+            </Link>
           </li>
           <li>
             {/* go to signUp page  */}
-            <Link to="">회원가입</Link>
+            <Link to="signup" className="content-box p-[10px] hover:text-red-500">
+              회원가입
+            </Link>
           </li>
-          <li>
-            {/* if signIn : user_profile */}
-            <img src="" alt="user_profile" />
-            <button onClick={''}>DropDownBtn</button>
-            <ul>
-              <li>
-                {/* go to profile page */}
-                <Link to="/profile"></Link>
-              </li>
-              <li>
-                {/* do logOut */}
-                <button onClick={''}>로그아웃</button>
-              </li>
-            </ul>
-          </li>
+          {/* <li> */}
+          {/* if signIn : user_profile */}
+          {/* <img src="" alt="user_profile" /> */}
+          {/* <button onClick={''}>DropDownBtn</button> */}
+          {/* <ul> */}
+          {/* <li> */}
+          {/* go to profile page */}
+          {/* <Link to="/profile"></Link> */}
+          {/* </li> */}
+          {/* <li> */}
+          {/* do logOut */}
+          {/* <button onClick={''}>로그아웃</button> */}
+          {/* </li> */}
+          {/* </ul> */}
+          {/* </li> */}
         </ul>
       </div>
     </header>
   );
 };
 export default Header;
-
