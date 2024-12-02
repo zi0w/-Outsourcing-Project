fa37988a05c8dd693ec6a3fae55e834c72411c79
Noon_sae <minseock7308@gmail.com>
2024-12-03 00:29:02 +0900

    feat: link chefTiles (black & white)

--- src/components/ui/home/Home.jsx
+++ src/components/ui/home/Home.jsx
@@ -1,27 +1,24 @@
 import React from 'react';
-// import BlackChefTiles from './BlackChefTiles';
-// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-  // const queryClient = new QueryClient();
+import BlackChefTiles from './BlackChefTiles';
+import WhiteChefTiles from './WhiteChefTiles';
 
 const Home = () => {
-
-
   return (
     <>
-      
       {/* Hero */}
-      <section className="bg-hero-image h-[700px] w-full bg-no-repeat bg-[0px_-260px] bg-cover indent-[-9999em]">
+      <section className="bg-hero-image h-[700px] w-full bg-no-repeat bg-[0px_-260px], bg-cover indent-[-9999em]">
         Hero_section
       </section>
 
       {/* BlackChefCards*/}
-      <section className="">        
+      <section className="bg-gradient-45-white">
+        <BlackChefTiles />
       </section>
 
       {/* WhiteChefCards */}
-      <section className="">
+      <section>
+        <WhiteChefTiles />
       </section>
-
     </>
   );
 };
