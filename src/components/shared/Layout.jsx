31f8f3e420f74929e8600fd578a9af5262927f54
Noon_sae <minseock7308@gmail.com>
2024-12-03 02:15:08 +0900

    fix: typo in the Layout.jsx

--- src/components/shared/Layout.jsx
+++ src/components/shared/Layout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom';
 
-import Header from '../layout/header';
+import Header from '../layout/Header';
 import Footer from '../layout/Footer';
 
 const Layout = () => {
