a02b4cff64a2e5e169f25129eb67d5ce1d71ea1f
Noon_sae <minseock7308@gmail.com>
2024-12-02 15:34:54 +0900

    feat: add hero section

---
+++ src/components/ui/home/Home.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+// import BlackChefTiles from './BlackChefTiles';
+// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+  // const queryClient = new QueryClient();
+
+const Home = () => {
+
+
+  return (
+    <>
+      
+      {/* Hero */}
+      <section className="bg-hero-image h-[700px] w-full bg-no-repeat bg-[0px_-260px] bg-cover indent-[-9999em]">
+        Hero_section
+      </section>
+
+      {/* BlackChefCards*/}
+      <section className="">        
+      </section>
+
+      {/* WhiteChefCards */}
+      <section className="">
+      </section>
+
+    </>
+  );
+};
+
+export default Home;
