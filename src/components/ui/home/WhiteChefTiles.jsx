14d5a772eebb27b853db76263ef1d531d5693d34
Noon_sae <minseock7308@gmail.com>
2024-12-03 00:29:31 +0900

    feat: WhiteChefTiles.jsx

---
+++ src/components/ui/home/WhiteChefTiles.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ChefTiles from './ChefTiles';
+
+const WhiteChefTiles = () => {
+  return (
+    <>
+      <ChefTiles bucketName="white_chefs_images" title="백수저 셰프님 식당 목록" />;
+    </>
+  );
+};
+
+export default WhiteChefTiles;
