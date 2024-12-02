ad0922f7e61b086aae16a401119fd3e1d9c71639
Noon_sae <minseock7308@gmail.com>
2024-12-02 09:55:51 +0900

    update: finalize footer layout and styling

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,7 +1,63 @@
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
+    <div className="bg-[#0a0a0a]">
+      <div className="mx-auto w-full max-w-[600px] h-[200px] p-[43px] text-center">
+        <p className="mb-[20px] text-[#fff] text-[21px] font-medium">
+          @ Copyright 2024 pa5rangers. All rights reserved
+        </p>
+        <ul className="flex justify-between mx-auto ">
+          <li className>
+            <p className="text-[#FF5555] font-medium">최민석</p>
+            <a
+              target="blank"
+              href="https://github.com/Noonsae"
+              className="block w-[42px] h-[42px] mx-auto indent-[-9999rem] bg-[url('/github.png')] bg-no-repeat bg-center bg-cover rounded-[50%]"
+            >
+              dev.Noonsae's github
+            </a>
+          </li>
+          <li>
+            <p className="text-[#F66CC8]">김민후</p>
+            <a
+              target="blank"
+              href="https://github.com/kminhoo"
+              className="block w-[42px] h-[42px] mx-auto indent-[-9999rem] bg-[url('/github.png')] bg-no-repeat bg-center bg-cover rounded-[50%]"
+            >
+              dev.kminhoo's github
+            </a>
+          </li>
+          <li>
+            <p className="text-[#ffff42]">김민지</p>
+            <a
+              target="blank"
+              href="https://github.com/minji7901"
+              className="block w-[42px] h-[42px] mx-auto indent-[-9999rem] bg-[url('/github.png')] bg-no-repeat bg-center bg-cover rounded-[50%]"
+            >
+              dev.minji7901's github
+            </a>
+          </li>
+          <li>
+            <p className="text-[#53FF67]">우지영</p>
+            <a
+              target="blank"
+              href="https://github.com/zi0w"
+              className="block w-[42px] h-[42px] mx-auto indent-[-9999rem] bg-[url('/github.png')] bg-no-repeat bg-center bg-cover rounded-[50%]"
+            >
+              dev.zi0w's github
+            </a>
+          </li>
+          <li>
+            <p className="text-[#455EFF]">최강건</p>
+            <a
+              target="blank"
+              href="https://github.com/Choi-kanggun"
+              className="block w-[42px] h-[42px] mx-auto indent-[-9999rem] bg-[url('/github.png')] bg-no-repeat bg-center bg-cover rounded-[50%]"
+            >
+              dev.Choi-kanggun's github
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
