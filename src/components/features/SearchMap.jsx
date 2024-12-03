87c9d36b8ed81e9d790ce9a384b6604c94ec214d
zi0w <zzz13588@naver.com>
2024-12-03 23:25:57 +0900

    feat: Add markers for all restaurants on Kakao Map

--- src/components/features/SearchMap.jsx
+++ src/components/features/SearchMap.jsx
@@ -1,11 +1,12 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import useRestaurants from '../../hooks/useRestaurants';
 
 const SearchMap = () => {
-  const address = '서울 성동구 성수일로6길 7-1';
+  const { restaurants } = useRestaurants();
   const { kakao } = window;
   const navigate = useNavigate();
-
+  const address = '경기도 안산시 상록구 성안길53';
   useEffect(() => {
     // 지도를 표시할 컨테이너
     const container = document.getElementById('map');
@@ -14,62 +15,67 @@ const SearchMap = () => {
       level: 3 // 확대 레벨
     };
 
-    // 지도 생성
+    // 지도 + geocoder 생성
     const map = new kakao.maps.Map(container, options);
     const geocoder = new kakao.maps.services.Geocoder();
 
-    // 주소 -> 좌표 변환
-    geocoder.addressSearch(address, (result, status) => {
-      if (status === kakao.maps.services.Status.OK) {
-        let coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+    restaurants.forEach((restaurant) => {
+      const { name, address, phone_number, id } = restaurant;
+      // 주소 -> 좌표 변환
+      geocoder.addressSearch(address, (result, status) => {
+        if (status === kakao.maps.services.Status.OK) {
+          const coords = new kakao.maps.LatLng(result[0].y, result[0].x);
 
-        // coords에 마커 생성
-        let marker = new kakao.maps.Marker({
-          map: map,
-          position: coords
-        });
+          // coords에 마커 생성
+          const marker = new kakao.maps.Marker({
+            map: map,
+            position: coords
+          });
 
-        // 커스텀 오버레이 생성
-        const overlayContent = document.createElement('div');
-        overlayContent.innerHTML = `
+          // 커스텀 오버레이 생성
+          const overlayContent = document.createElement('div');
+          overlayContent.innerHTML = `
           <div style="width: 16rem; padding: .75rem; background: white; border: .5rem; border-radius: .5rem; box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1);">
             <div style="display: flex; align-items: flex-start; justify-content: space-between; margin-bottom: .5rem;">
-              <h3 style="font-weight: 600; font-size: 1.125rem; font-color: #1F2937; line-height: 1.75rem">중앙감속기</h3>
+              <h3 style="font-weight: 600; font-size: 1.125rem; font-color: #1F2937; line-height: 1.75rem">${name}</h3>
               <div id="close-overlay" style="font-weight: 700; cursor: pointer;">&#10005;</div>
             </div>
             <div style="font-color:#4B5563;">
-              <p style="margin-bottom: .1rem; font-size: .875rem; overflow-wrap: break-word; text-wrap:wrap; break-word: keep-all">서울 성동구 성수일로6길 7-1</p>
-              <span style="font-size: .875rem;">02-123-4567</span>
+              <p style="margin-bottom: .1rem; font-size: .875rem; overflow-wrap: break-word; text-wrap:wrap; break-word: keep-all">${address}</p>
+              <span style="font-size: .875rem;">${phone_number}</span>
             </div>
-            <button style="width:100%; margin-top:.75rem; border-radius: .5rem; background-color:black; text-align: center; padding: .5rem 1rem .5rem 1rem; color:white; cursor: pointer;">
+            <button id="detail-overlay" style="width:100%; margin-top:.75rem; border-radius: .5rem; background-color:black; text-align: center; padding: .5rem 1rem .5rem 1rem; color:white; cursor: pointer;">
               자세히 보기
             </button>
           </div>
         `;
 
-        const overlay = new kakao.maps.CustomOverlay({
-          content: overlayContent,
-          position: coords,
-          map,
-          yAnchor: 1.4
-        });
+          const overlay = new kakao.maps.CustomOverlay({
+            content: overlayContent,
+            position: coords,
+            map,
+            yAnchor: 1.4
+          });
 
-        overlay.setMap(null); // 초기에는 표시하지 않음
+          overlay.setMap(null); // 초기에는 표시하지 않음
 
-        kakao.maps.event.addListener(marker, 'click', () => {
-          overlay.setMap(map);
-        });
+          kakao.maps.event.addListener(marker, 'click', () => {
+            overlay.setMap(map);
+          });
 
-        const closeButton = overlayContent.querySelector('#close-overlay');
-        console.log(closeButton);
-        closeButton.addEventListener('click', () => {
-          overlay.setMap(null);
-        });
+          const closeButton = overlayContent.querySelector('#close-overlay');
+          closeButton.addEventListener('click', () => {
+            overlay.setMap(null);
+          });
 
-        map.setCenter(coords);
-      }
+          const detailButton = overlayContent.querySelector('#detail-overlay');
+          detailButton.addEventListener('click', () => {
+            navigate(`/detail/${id}`);
+          });
+        }
+      });
     });
-  }, [kakao, address]);
+  }, [kakao, address, navigate]);
 
   return (
     <>
