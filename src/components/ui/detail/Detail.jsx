4b5d0bae187748f61940aefe0cc3bc40db00de30
kminhoo <whdgus4158@gmail.com>
2024-12-03 11:09:06 +0900

    chore: remove comment

--- src/components/ui/detail/Detail.jsx
+++ src/components/ui/detail/Detail.jsx
@@ -22,48 +22,7 @@ const Detail = () => {
             </div>
 
             <div className="w-full h-[760px] rounded-[24px] px-[40px] pt-[38.12px] pb-[35.77px] bg-white overflow-hidden">
-              {/* <div>
-                <h4 className="text-lg font-[600]">리뷰</h4>
-
-                <form>
-                  <textarea
-                    placeholder="리뷰를 입력해주세요."
-                    className="mt-[9.86px] p-3 text-sm max-w-[440px] w-full h-[142.97px] rounded-[16px] border-none outline-none shadow-md resize-none"
-                  />
-                  <button
-                    type="sumbit"
-                    className="mt-[18.73px] w-full h-[47.66px] border-none bg-[#EC4C4C] rounded-[16px] text-white font-[600] hover:bg-[#B73838] transition-all"
-                  >
-                    리뷰 작성하기
-                  </button>
-                </form>
-              </div> */}
               <CommentForm id={'0c67a5e8-eef2-4995-a5bf-a14d6910ad0a'} />
-
-              {/* <div className="mt-[30px] border-t-2 border-[#AAAAAA]">
-                <div className="p-2 flex flex-col gap-[27px] mt-[8px] max-h-[300px] overflow-y-auto">
-                  <div className="w-full min-h-[120px] p-2 rounded-[16px] border-none shadow-md flex flex-row items-center justify-center gap-[9.98px]">
-                    <img alt="User Profile" className="w-[60px] h-[60px] bg-red-400 rounded-full" />
-                    <div>
-                      <div className="flex items-center justify-between">
-                        <h5 className="text-base font-[600]">아이디 혹은 이메일</h5>
-                        <p className="text-[12px] text-slate-300">2024. 12. 01, 11:36</p>
-                      </div>
-                      <div className="mt-[6.67px] flex items-center justify-between">
-                        <p className="text-sm">나야 들기름...멘트 실제로 들어보고 싶어서 갔어요!</p>
-                      </div>
-                      <div className="mt-[8px] flex gap-2 items-center justify-end">
-                        <button className="w-[50px] h-[30px] border-none outline-none rounded-[5px] text-white text-sm bg-blue-600">
-                          수정
-                        </button>
-                        <button className="w-[50px] h-[30px] border-none outline-none rounded-[5px] text-white text-sm bg-red-600">
-                          삭제
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div> */}
               <Comments id={'0c67a5e8-eef2-4995-a5bf-a14d6910ad0a'} />
             </div>
           </div>
