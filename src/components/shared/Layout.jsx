c4caaeedae210b764d4baf77432aeaa991aea612
minji7901 <kimem7901@gmail.com>
2024-12-04 23:00:11 +0900

    feat : add condition to hide Footer

--- src/components/shared/Layout.jsx
+++ src/components/shared/Layout.jsx
@@ -1,16 +1,18 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 
 import Header from '../layout/Header';
 import Footer from '../layout/Footer';
 
 const Layout = () => {
+  const location = useLocation();
+
   return (
     <>
       <Header />
       <main>
         <Outlet />
       </main>
-      <Footer />
+      {location.pathname === '/search' || location.pathname.includes('/detail') ? null : <Footer />}
     </>
   );
 };
