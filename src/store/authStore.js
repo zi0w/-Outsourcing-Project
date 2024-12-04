d57b92fd504acd8e1a3b5563edb4d609cda6127a
minji7901 <kimem7901@gmail.com>
2024-12-04 10:16:37 +0900

    feat : google login implementing

--- src/store/authStore.js
+++ src/store/authStore.js
@@ -47,6 +47,48 @@ const authStore = persist(
       }
     },
 
+    // 소셜 로그인 (구글)
+    googleLogin: async () => {
+      try {
+        //supabase 구글로그인 요청
+        const { data: googleUser, error } = await supabase.auth.signInWithOAuth({
+          provider: 'google',
+          options: {
+            queryParams: {
+              access_type: 'offline',
+              prompt: 'consent'
+            }
+          }
+        });
+        const { user } = googleUser;
+
+        if (error) throw new Error(error.message);
+        
+        // users 테이블에서 사용자 정보 가져오기
+        const { data, error: userError } = await supabase
+          .from('users')
+          .select('id, nickname, profile_image_url')
+          .eq('email', user.email)
+          .single();
+        if (userError) throw new Error(userError.message);
+
+        // 상태 업데이트
+        set({
+          isLogin: true,
+          user: {
+            id: data?.id,
+            email: user?.email,
+            nickname: data?.nickname,
+            profile_image_url: data?.profile_image_url
+          }
+        });
+        console.log('Google login successful, state updated');
+      } catch (error) {
+        console.error('Google login error:', error.message);
+        throw new Error(error.message);
+      }
+    },
+
     //로그아웃
     logout: () => {
       // 상태 초기화
