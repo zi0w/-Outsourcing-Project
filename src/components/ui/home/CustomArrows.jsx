d15b1bd19f38741e68e386fa005f349e146383cc
Noon_sae <minseock7308@gmail.com>
2024-12-04 20:15:45 +0900

    chore: fix typo

--- src/components/ui/home/CustomArrows.jsx
+++ src/components/ui/home/CustomArrows.jsx
@@ -1,11 +1,35 @@
+// White Custom Prev Arrow
+const WhiteCustomPrevArrow = ({ onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      className="w-8 h-8 flex items-center justify-center absolute top-1/2 left-[-20px] transform -translate-y-[100%] p-4 rounded-full shadow-[0px_2px_2px_rgba(0,0,0,0.3)] z-30 text-black bg-white text-center"
+    >
+      ◀
+    </button>
+  );
+};
+
+// White Custom Next Arrow
+const WhiteCustomNextArrow = ({ onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      className="w-8 h-8 flex items-center justify-center absolute top-1/2 right-[-20px] transform -translate-y-[100%] p-4 rounded-full shadow-[0px_2px_2px_rgba(0,0,0,0.3)] z-30 text-black bg-white text-center"
+    >
+      ▶
+    </button>
+  );
+};
+
 // Black Custom Prev Arrow
 const BlackCustomPrevArrow = ({ onClick }) => {
   return (
     <button
       onClick={onClick}
-      className="w-8 h-8 flex items-center justify-center absolute top-1/2 left-[-20px] transform -translate-y-[100%] p-4 rounded-full shadow-[0px_2px_2px_rgba(0,0,0,0.3)] z-30 text-black bg-white"
+      className="w-8 h-8 flex items-center justify-center absolute top-1/2 left-[-20px] transform -translate-y-[100%] p-4 rounded-full shadow-[0px_2px_2px_rgba(0,0,0,0.3)] z-30 text-white bg-black text-center"
     >
-      ←
+      ◀
     </button>
   );
 };
@@ -15,32 +39,9 @@ const BlackCustomNextArrow = ({ onClick }) => {
   return (
     <button
       onClick={onClick}
-      className="w-8 h-8 flex items-center justify-center absolute top-1/2 right-[-20px] transform -translate-y-[100%] p-4 rounded-full shadow-[0px_2px_2px_rgba(0,0,0,0.3)] z-30 text-black bg-white"
+      className="w-8 h-8 flex items-center justify-center absolute top-1/2 right-[-20px] transform -translate-y-[100%] text-white bg-black p-4 rounded-full shadow-[0px_2px_2px_rgba(0,0,0,0.3)] z-30 text-center"
     >
-      →
-      {/* <img className="block w-8 h-8 border object-full" src={right_arrow} alt="right_arrow" /> */}
-    </button>
-  );
-};
-
-// White Custom Prev Arrow
-const WhiteCustomPrevArrow = ({ onClick }) => {
-  return (
-    <button
-      onClick={onClick}
-      className="w-8 h-8 flex items-center justify-center absolute top-1/2 left-[-20px] transform -translate-y-[100%] p-4 rounded-full shadow-[0px_2px_2px_rgba(0,0,0,0.3)] z-30 text-white bg-black"
-    >←</button>
-  );
-};
-
-// White Custom Next Arrow
-const WhiteCustomNextArrow = ({ onClick }) => {
-  return (
-    <button
-      onClick={onClick}
-      className="w-8 h-8 flex items-center justify-center absolute top-1/2 right-[-20px] transform -translate-y-[100%] text-white bg-black p-4 rounded-full shadow-[0px_2px_2px_rgba(0,0,0,0.3)] z-30]"
-    >
-      →
+      ▶
     </button>
   );
 };
