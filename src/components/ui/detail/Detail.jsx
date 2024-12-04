7ed6807f1c35ef483a0b62f14ee630a9605a80cf
kminhoo <whdgus4158@gmail.com>
2024-12-04 10:27:08 +0900

    fix: useParams add

--- src/components/ui/detail/Detail.jsx
+++ src/components/ui/detail/Detail.jsx
@@ -1,26 +1,30 @@
-import Comments from './Comments';
+import { useParams } from 'react-router-dom';
 
 import CommentForm from '../../features/CommentForm';
 import DetailMap from '../../features/DetailMap';
+
+import Comments from './Comments';
 import RestaurantInfo from './RestaurantInfo';
 
 const Detail = () => {
+  const { id } = useParams();
+
   return (
     <section>
       <div className="m-auto max-w-[1440px] w-full h-screen bg-black">
         <div className="w-full h-full p-[40px] flex flex-row gap-[40px]">
           {/* 식당 정보 */}
           <div className="max-w-[520px] w-full flex flex-col gap-[40px]">
-            <RestaurantInfo id={'0c67a5e8-eef2-4995-a5bf-a14d6910ad0a'} />
+            <RestaurantInfo id={id} />
 
             <div className="w-full h-[760px] rounded-[24px] px-[40px] pt-[38.12px] pb-[35.77px] bg-white overflow-hidden relative">
-              <CommentForm id={'0c67a5e8-eef2-4995-a5bf-a14d6910ad0a'} />
-              <Comments id={'0c67a5e8-eef2-4995-a5bf-a14d6910ad0a'} />
+              <CommentForm id={id} />
+              <Comments id={id} />
             </div>
           </div>
 
           {/* 식당 위치 */}
-          <DetailMap id={'0c67a5e8-eef2-4995-a5bf-a14d6910ad0a'} />
+          <DetailMap id={id} />
         </div>
       </div>
     </section>
