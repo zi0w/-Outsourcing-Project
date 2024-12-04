8a8a7978395a3b165a0bf049535f64111aaad888
Noon_sae <minseock7308@gmail.com>
2024-12-04 09:00:18 +0900

    style: adjust font weight

--- src/components/ui/home/BlackChefsSection.jsx
+++ src/components/ui/home/BlackChefsSection.jsx
@@ -7,7 +7,7 @@ const BlackChefsSection = () => {
         <div className="">
           <h2 className="text-center text-[26px] font-bold  mb-[40px]">흑수저 셰프님 식당 목록 </h2>
           <BlackChefTiles />
-          <p className="text-center mt-[60px] text-[21px] font-Medium">
+          <p className="text-center mt-[60px] text-[21px] font-bold">
             사진을 클릭하면 셰프님들의 가게 위치를 지도에서 확인하실 수 있습니다.
           </p>
         </div>
