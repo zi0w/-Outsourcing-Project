a4196e1a74197bbfa20069c15fe899f01c00790a
Noon_sae <minseock7308@gmail.com>
2024-12-04 09:02:04 +0900

    chore: fix typo

--- src/components/ui/search/SearchList.jsx
+++ src/components/ui/search/SearchList.jsx
@@ -10,7 +10,6 @@ const SearchList = ({ restaurant }) => {
       </div>
       <div>
         <span className="border border-black rounded-full p-2 w-[30px] h-[30px] flex items-center justify-center bg-black text-white">
-          >
         </span>
       </div>
     </li>
