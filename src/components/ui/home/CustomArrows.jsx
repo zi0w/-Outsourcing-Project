991bdf40f8c5198a848fa96be62d08116fd0eef8
Noon_sae <minseock7308@gmail.com>
2024-12-04 10:15:08 +0900

    chore: fix typo

--- src/components/ui/home/CustomArrows.jsx
+++ src/components/ui/home/CustomArrows.jsx
@@ -8,7 +8,7 @@ const CustomPrevArrow = ({ onClick, color }) => {
       onClick={onClick}
       color={color}
       className={`w-8 h-8 flex items-center justify-center absolute top-1/2 left-[-20px] transform -translate-y-[100%]  text-white p-4 rounded-full shadow-[0px_2px_2px_rgba(0,0,0,0.3)]  z-30 
-      ${color === 'black' ? 'bg-black text-white' : 'bag-white text-black '}`}
+      ${color === 'black' ? 'bg-black text-white' : 'bg-white text-black '}`}
     >
       ←
     </button>
@@ -22,7 +22,7 @@ const CustomNextArrow = ({ onClick, color }) => {
       onClick={onClick}
       color={color}
       className={`w-8 h-8 flex items-center justify-center absolute top-1/2 right-[-20px] transform -translate-y-[100%]  text-white p-4 rounded-full shadow-[0px_2px_2px_rgba(0,0,0,0.3)] z-30 
-      ${color === 'black' ? 'bg-black text-white' : 'bag-white text-black '}`}
+      ${color === 'black' ? 'bg-black text-white' : 'bg-white text-black '}`}
     >
       →
     </button>
