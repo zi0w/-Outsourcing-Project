e99ad5f89a786f2d8a9989599dcd172d46c505cb
Noon_sae <minseock7308@gmail.com>
2024-12-04 19:51:13 +0900

    feat: add custom arrow

--- src/components/ui/home/ChefCardList.jsx
+++ src/components/ui/home/ChefCardList.jsx
@@ -4,10 +4,9 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
-import { CustomPrevArrow, CustomNextArrow } from './CustomArrows';
-
 import ChefCardItem from './ChefCardItem';
 import SliderSelector from './SliderSelector';
+import { BlackCustomNextArrow, BlackCustomPrevArrow, WhiteCustomNextArrow, WhiteCustomPrevArrow } from './CustomArrows';
 
 const ChefCardList = ({ restaurantInfo, color }) => {
   const sliderRef = useRef(null);
@@ -15,14 +14,38 @@ const ChefCardList = ({ restaurantInfo, color }) => {
   const filteredInfo = restaurantInfo.filter((item) => item.color === color);
 
   // slider settings
-  const settings = {
+  const whiteSettings = {
     dots: false, // 하단 인디케이터 표시
     infinite: true, // 무한 반복
     speed: 700, // 슬라이드 전환 속도
     slidesToShow: 3, // 한 화면에 보여줄 슬라이드 수
     slidesToScroll: 3, // 한 번에 스크롤할 슬라이드 수
-    prevArrow: <CustomPrevArrow />,
-    nextArrow: <CustomNextArrow />,
+    prevArrow: <BlackCustomPrevArrow />,
+    nextArrow: <BlackCustomNextArrow />,
+    responsive: [
+      {
+        breakpoint: 1024, // 화면 크기가 1024px 이하일 때
+        settings: {
+          slidesToShow: 2 // 슬라이드 2개 표시
+        }
+      },
+      {
+        breakpoint: 640, // 화면 크기가 640px 이하일 때
+        settings: {
+          slidesToShow: 1 // 슬라이드 1개 표시
+        }
+      }
+    ]
+  };
+
+  const blackSettings = {
+    dots: false, // 하단 인디케이터 표시
+    infinite: true, // 무한 반복
+    speed: 700, // 슬라이드 전환 속도
+    slidesToShow: 3, // 한 화면에 보여줄 슬라이드 수
+    slidesToScroll: 3, // 한 번에 스크롤할 슬라이드 수
+    prevArrow: <WhiteCustomPrevArrow />,
+    nextArrow: <WhiteCustomNextArrow />,
     responsive: [
       {
         breakpoint: 1024, // 화면 크기가 1024px 이하일 때
@@ -48,7 +71,7 @@ const ChefCardList = ({ restaurantInfo, color }) => {
 
       {/* Slider */}
       <div className=" mx-auto max-w-[1440px] ">
-        <Slider ref={sliderRef} {...settings}>
+        <Slider ref={sliderRef} {...(color === 'black' ? whiteSettings :  blackSettings)}>
           {/* ChefCard */}
           {filteredInfo.map((info) => (
             <ChefCardItem key={info.id} info={info} color={color} />
--- src/components/ui/home/CustomArrows.jsx
+++ src/components/ui/home/CustomArrows.jsx
@@ -1,32 +1,52 @@
 import React from 'react';
 
-// 커스텀 Prev 버튼
-const CustomPrevArrow = ({ onClick, color }) => {
-
+// Black Custom Prev Arrow
+const BlackCustomPrevArrow = ({ onClick }) => {
   return (
     <button
       onClick={onClick}
-      color={color}
-      className={`w-8 h-8 flex items-center justify-center absolute top-1/2 left-[-20px] transform -translate-y-[100%]  text-white p-4 rounded-full shadow-[0px_2px_2px_rgba(0,0,0,0.3)]  z-30 
-      ${color === 'black' ? 'bg-black text-white' : 'bg-white text-black '}`}
+      className="w-8 h-8 flex items-center justify-center absolute top-1/2 left-[-20px] transform -translate-y-[100%] p-4 rounded-full shadow-[0px_2px_2px_rgba(0,0,0,0.3)] z-30 text-black bg-white"
     >
       ←
     </button>
   );
 };
 
-// 커스텀 Next 버튼
-const CustomNextArrow = ({ onClick, color }) => {
+// Black Custom Next Arrow
+const BlackCustomNextArrow = ({ onClick }) => {
   return (
     <button
       onClick={onClick}
-      color={color}
-      className={`w-8 h-8 flex items-center justify-center absolute top-1/2 right-[-20px] transform -translate-y-[100%]  text-white p-4 rounded-full shadow-[0px_2px_2px_rgba(0,0,0,0.3)] z-30 
-      ${color === 'black' ? 'bg-black text-white' : 'bg-white text-black '}`}
+      className="w-8 h-8 flex items-center justify-center absolute top-1/2 right-[-20px] transform -translate-y-[100%] p-4 rounded-full shadow-[0px_2px_2px_rgba(0,0,0,0.3)] z-30 text-black bg-white"
+    >
+      →
+      {/* <img className="block w-8 h-8 border object-full" src={right_arrow} alt="right_arrow" /> */}
+    </button>
+  );
+};
+
+// White Custom Prev Arrow
+const WhiteCustomPrevArrow = ({ onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      className="w-8 h-8 flex items-center justify-center absolute top-1/2 left-[-20px] transform -translate-y-[100%] p-4 rounded-full shadow-[0px_2px_2px_rgba(0,0,0,0.3)] z-30 text-white bg-black"
+    >
+      ←
+    </button>
+  );
+};
+
+// White Custom Next Arrow
+const WhiteCustomNextArrow = ({ onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      className="w-8 h-8 flex items-center justify-center absolute top-1/2 right-[-20px] transform -translate-y-[100%] text-white bg-black p-4 rounded-full shadow-[0px_2px_2px_rgba(0,0,0,0.3)] z-30]"
     >
       →
     </button>
   );
 };
 
-export { CustomPrevArrow, CustomNextArrow };
+export { BlackCustomPrevArrow, BlackCustomNextArrow, WhiteCustomPrevArrow, WhiteCustomNextArrow };
